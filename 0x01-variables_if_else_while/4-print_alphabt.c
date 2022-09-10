45790a0b76e491c9392c4d4f6c62d28556237217
Dolybim <dolybim2gud@gmail.com>
2022-09-10 12:43:48 -0700

    Write a program that prints the alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio>
+
+/**
+ * main - print all the letters of the alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 97;
+
+	while (i < 123)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+		i++;
+	}
+	putchar(10);
+
+	return (0);
+}
